065a4c730d1e57c7f108e752a4c6f22f6cac36cb
jack dunning and steven fleming <jack@>
2020-01-29 15:33:12 -0800

    Add all game functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,39 @@
 //BackEnd
 
-var p1Scores = {
-    total: 0,
-    turn: 0,
-    roll: 0,
+var playerScores = {
+    p1Total: 0,
+    p2Total: 0,
 }
 
+
+var turnScores = {
+    runningTotal: 0,
+    roll: 0,
+    turnCount: 1,
+}
+
+function turnCount() {
+    turnScores.turnCount++
+    if(turnScores.turnCount % 2 == 0){
+        console.log("player 2 turn")
+        $("#playerTwoTurn").show();
+        $("#playerOneTurn").hide();
+    }
+    else {
+        console.log("player 1 turn")
+        $("#playerOneTurn").show();
+        $("#playerTwoTurn").hide();
+    }
+}
+    function newGame() {
+        turnScores.runningTotal= 0
+        turnScores.roll = 0
+        turnScores.turnCount = 0
+        playerScores.p1Total= 0
+        playerScores.p2Total= 0
+        turnScores.turnCount =2
+
+}
 
 function getRoll() {
     var min = 1;
@@ -13,40 +41,35 @@ function getRoll() {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-// Player 2 functions
-
-function addRollToTurn2() {
-    p2Scores.roll = getRoll();
-    if (p2cores.roll !=1) {
-        return p2Scores.turn += p1Scores.roll;
+function addRollToRunningTotal() {
+    turnScores.roll = getRoll();
+    if (turnScores.roll != 1) {
+        return turnScores.runningTotal += turnScores.roll;
     } else { 
-        return p2Scores.turn = 0;
+        turnCount()
+        return turnScores.runningTotal = 0;
     }
 }
 
-function addTotal2() {
-    p2Scores.turn += p2Scores.total;
-    p2Scores.turn = 0;
-    if (p1Scores.total >= 100) {
-    $("#winner").text("Player 2 Winner")
-  
-    }
-}
-//Player one functions
-function addRollToTurn() {
-    p1Scores.roll = getRoll();
-    if (p1Scores.roll !=1) {
-        return p1Scores.turn += p1Scores.roll;
-    } else { 
-        return p1Scores.turn = 0;
-    }
-}
 
-function addTotal(player){
-    player.total += p1Scores.turn;
-    p1Scores.turn = 0;
-    if (p1Scores.total >= 100) {
-        $("#winner").text("Player 1 Winner")
+
+function addTotal(){
+    // need to write if statment telling computer whether to add running total to p1 or p2 
+
+    if(turnScores.turnCount % 2 != 0) {
+
+        playerScores.p1Total += turnScores.runningTotal;
+        turnScores.runningTotal = 0;
+        if (playerScores.p1Total >= 100) {
+            $("#winner").text("Player 1 Winner")
+        } 
+    } else {
+        playerScores.p2Total += turnScores.runningTotal;
+        console.log(playerScores.p2Total)
+        turnScores.runningTotal = 0;
+        if (playerScores.p2Total >= 100) {
+            $("#winner").text("Player 2 Winner")
+        }
     }
 }
 
@@ -55,21 +78,34 @@ $(document).ready(function(){
 
     //Frontend
 
-    $("#roll").click(function(){
-        p1Scores.roll = getRoll();
-        addRollToTurn();
-        $("#rollBox").text(p1Scores.roll);
+    //Developing Start New game funcitonality
+    $("#start").click(function(){
+        newGame()
+        $("#p1Total").text(playerScores.p1Total);
+        $("#p2Total").text(playerScores.p2Total);
+        $("#p1Roll").text(turnScores.roll);
+        $("#p1Turn").text(turnScores.runningTotal);
+        $("#p2Roll").text(turnScores.roll);
+        $("#p2Turn").text(turnScores.runningTotal);
+    })
+    // 
 
-        $("#p1Roll").text(p1Scores.roll)
-        $("#p1Turn").text(p1Scores.turn)
+    $("#roll").click(function(){
+        addRollToRunningTotal();
+        
+        $("#p1Roll").text(turnScores.roll);
+        $("#p1Turn").text(turnScores.runningTotal);
+        $("#p2Roll").text(turnScores.roll);
+        $("#p2Turn").text(turnScores.runningTotal);
         
     })
 
 
     $("#hold").click(function(){
         addTotal();  
-        $("#p1Total").text(p1Scores.total)
-        $("#p1Turn").text(p1Scores.turn)
+        $("#p1Total").text(playerScores.p1Total);
+        $("#p2Total").text(playerScores.p2Total);
+        turnCount()
     })
 
 })
